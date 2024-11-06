4817ccd756cb6ddd7caa238c203ad6ef9fa75571
Patrice Torguet <torguet@gmail.com>
2024-11-06 13:16:35 +0100

    Suppression de la TZID dupliquée. Chargement ok sur Google Calendar.

--- src/main/java/net/torguet/cal/ICSGenerator.java
+++ src/main/java/net/torguet/cal/ICSGenerator.java
@@ -1,11 +1,7 @@
 package net.torguet.cal;
 
 import net.fortuna.ical4j.model.Calendar;
-import net.fortuna.ical4j.model.Property;
-import net.fortuna.ical4j.model.TimeZoneRegistry;
 import net.fortuna.ical4j.model.component.VEvent;
-import net.fortuna.ical4j.model.parameter.TzId;
-import net.fortuna.ical4j.model.parameter.Value;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -21,7 +17,7 @@ public class ICSGenerator {
 
     public void generate() throws FileNotFoundException {
         Calendar copyCalendar = new Calendar();
-        TzId tzParam = new TzId(TimeZoneRegistry.getGlobalZoneId("Europe/Paris").getId());
+        //TzId tzParam = new TzId(TimeZoneRegistry.getGlobalZoneId("Europe/Paris").getId());
 
         for(Semaine semaine : calendar.getSemaines()) {
             if (semaine == null)
@@ -38,11 +34,11 @@ public class ICSGenerator {
 
                     String resume = cours.toString();
 
-                    VEvent week1UserA = new VEvent(start, end, resume);
-                    week1UserA.getRequiredProperty(Property.DTSTART).add(tzParam).add(Value.DATE);
-                    week1UserA.getRequiredProperty(Property.DTEND).add(tzParam).add(Value.DATE);
+                    VEvent event = new VEvent(start, end, resume);
+                    //event.getRequiredProperty(Property.DTSTART).add(tzParam).add(Value.DATE);
+                    //event.getRequiredProperty(Property.DTEND).add(tzParam).add(Value.DATE);
 
-                    copyCalendar.add(week1UserA);
+                    copyCalendar.add(event);
                 }
             }
         }

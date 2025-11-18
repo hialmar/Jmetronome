35039c36035b52fef14c9d92c4e74d0b6f7cdef6
Patrice Torguet <torguet@gmail.com>
2025-11-18 11:15:19 +0100

    Lecture partielle de l'EDT M1 MIAGE.

--- src/main/java/net/torguet/xlsx/miage/M1Reader.java
+++ src/main/java/net/torguet/xlsx/miage/M1Reader.java
@@ -1,16 +1,19 @@
 package net.torguet.xlsx.miage;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import net.torguet.cal.*;
 import net.torguet.xlsx.list.ListWriter;
-import net.torguet.xlsx.old.ColorReader;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.*;
 
 import java.io.*;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
 
 import static org.apache.poi.ss.usermodel.CellType.*;
 
@@ -26,32 +29,29 @@ public class M1Reader {
     private final int[] debutCours = new int[6];
     private final int[] debutCoursHoraires = new int[6];
     private int nbDebutCours = 0;
-    private int[] debutTD = new int[6];
-    private String[] groupeTDNames = new String[6];
-    private int[] debutTP = new int[6];
-    private String[] groupeTPNames = new String[6];
+    private final int[] debutTD = new int[6];
+    private final String[] groupeTDNames = new String[6];
+    private final int[] debutTP = new int[6];
+    private final String[] groupeTPNames = new String[6];
     private int nbGroupesTD = 0;
     private int nbGroupesTP = 0;
 
     private ZonedDateTime date;
     private Semaine semaine;
 
-    private final ColorReader colorReader;
-
     private int rowNumber;
     private int colNumber;
+    private int jourSemaine;
 
     public M1Reader(String fileName, int sheetNumber) throws Exception {
         calendar = new Calendrier();
         fis = new FileInputStream(fileName);
         XSSFWorkbook workbook = new XSSFWorkbook(fis);
         sheet = workbook.getSheetAt(sheetNumber);
-        colorReader = new ColorReader(fileName, sheetNumber);
     }
 
     public void close() throws IOException, InterruptedException {
         fis.close();
-        colorReader.close();
     }
 
     public Calendrier traiterFichier() {
@@ -155,16 +155,8 @@ public class M1Reader {
                 while (cell != null && cell.getCellType() == STRING) {
                     String value = chercheHoraire(cell);
                     debutCours[debutCoursIdx] = rowNumber-debutCoursLundi;
-                    int idxH = value.indexOf("h");
-                    int heure = 0;
-                    int minute = 0;
-                    if (idxH != -1) {
-                        heure = Integer.parseInt(value.substring(0, idxH));
-                    }
-                    if (idxH < value.length()-2) {
-                        minute = Integer.parseInt(value.substring(idxH+1));
-                    }
-                    debutCoursHoraires[debutCoursIdx] = heure*100+minute;
+                    HeureMinute result = getHeureMinute(value);
+                    debutCoursHoraires[debutCoursIdx] = result.heure() *100+ result.minute();
                     System.out.println(debutCours[debutCoursIdx]+"->"+debutCoursHoraires[debutCoursIdx]);
                     debutCoursIdx++;
                     rowNumber++;
@@ -177,6 +169,22 @@ public class M1Reader {
             }
             idJour++;
         }
+    }
+
+    private static HeureMinute getHeureMinute(String value) {
+        int idxH = value.indexOf("h");
+        int heure = 0;
+        int minute = 0;
+        if (idxH != -1) {
+            heure = Integer.parseInt(value.substring(0, idxH));
+        }
+        if (idxH < value.length()-2) {
+            minute = Integer.parseInt(value.substring(idxH+1));
+        }
+        return new HeureMinute(heure, minute);
+    }
+
+    private record HeureMinute(int heure, int minute) {
     }
 
     private void chercheJourSemaine(int idJour) {
@@ -236,17 +244,17 @@ public class M1Reader {
             semaine = new Semaine((int) cell.getNumericCellValue());
 
             System.out.println("Semaine "+ cell.getNumericCellValue());
-            int nbJour = 0;
             rowNumber = debutCoursLundi;
+            jourSemaine = 0;
             while (rowNumber < sheet.getPhysicalNumberOfRows()) {
                 System.out.println(date);
-                Jour jour = recupJour(nbJour);
+                Jour jour = recupJour();
                 if (jour != null) {
                     semaine.addJour(jour);
                 }
-                nbJour++;
+                jourSemaine++;
                 date = date.plusDays(1);
-                if(nbJour>=5) {
+                if(jourSemaine>=5) {
                     break;
                 }
             }
@@ -259,13 +267,13 @@ public class M1Reader {
     }
 
 
-    private Jour recupJour(int nbJour) {
+    private Jour recupJour() {
         Jour jour = new Jour(date);
         int nbCours = 0;
-        for(int coursIdx = 0; coursIdx <nbDebutCours ; coursIdx++) {
+        for(int coursIdx = 0; coursIdx < nbDebutCours ; coursIdx++) {
             // créneau Cours ou TD1 ou TP1
             int row = rowNumber+debutCours[coursIdx];
-            System.out.println("Cours Ligne :"+row);
+            System.out.println("Cours Ligne :"+row+", jour : "+joursSemaine[jourSemaine]+" horaire : "+debutCoursHoraires[coursIdx]);
             var cell = sheet.getRow(row).getCell(colNumber);
             if (cell != null && cell.getCellType() == STRING) {
                 Cours cours = recupCours(cell);
@@ -286,6 +294,7 @@ public class M1Reader {
             }
         }
         rowNumber += debutCours[nbDebutCours-1]+debutTP[nbGroupesTP-1];
+        rowNumber++; // saute le jour de la semaine
         if (nbCours == 0) {
             return null;
         }
@@ -308,47 +317,75 @@ public class M1Reader {
     }
 
     private Cours recupCours(XSSFCell cell) {
-        Cours cours = null;
+        Cours cours;
         System.out.println("recupCours");
 
         String value = cell.getStringCellValue();
 
-        String [] split = value.split("[-\\n]");
+        String [] split = value.trim().split("[-\\n]");
+        // remove empty strings
+        List<String> list = new ArrayList<>(Arrays.asList(split));
+        list.removeIf(item -> item == null || item.isEmpty());
+        split = list.toArray(new String[0]);
 
         if (split.length > 1) {
             cours = new Cours(split[1]);
-            cours.setCodeApogee(split[0]);
-            if (split.length > 2) {
-                switch (split[2]) {
-                    case "Cours":
-                        cours.setType(TypeCours.TYPE_COURS);
-                        break;
-                    case "TD":
-                    case "TD1":
-                    case "TD2":
-                        cours.setType(TypeCours.TYPE_TD);
-                        break;
-                    case "TP":
-                        cours.setType(TypeCours.TYPE_TP);
-                        break;
-                    default:
-                        cours.setType(TypeCours.TYPE_AUTRE);
+            // si la premiere chaine est un code apogée
+            if (split[0].startsWith("KMIA")) {
+                cours.setCodeApogee(split[0]);
+                if (split.length > 2) {
+                    setTypeCours(split[2], cours);
+                }
+                if (split.length > 3) {
+                    cours.setEnseignant(split[3]);
+                }
+            } else if (split[0].matches(timeMatcher)) {
+                if (split[1].matches(timeMatcher)) {
+                    // calcul de la longueur
+                    HeureMinute hmDebut = getHeureMinute(split[0]);
+                    HeureMinute hmFin = getHeureMinute(split[1]);
+                    double longueur = hmFin.heure + hmFin.minute/60.0 - hmDebut.heure - hmDebut.minute/60.0;
+                    cours.setDuree((float)longueur);
+                    if (split.length > 2) {
+                        setTypeCours(split[2], cours);
+                    }
+                    if (split.length > 3) {
+                        cours.setIntitule(split[3]);
+                    }
+                }
+            } else if (split[0].equalsIgnoreCase("Conférence")) {
+                if (split.length > 2) {
+                    cours.setEnseignant(split[2]);
+                    cours.setIntitule(split[0] + " : " + split[1] + " " + split[2]);
+                    if (split.length > 3) {
+                        cours.setEnseignant(split[3]);
+                    }
+                } else {
+                    cours.setIntitule(split[0] + " : " + split[1]);
+                }
+            } else { // sinon ce doit être un intitulé
+                cours.setIntitule(split[0]);
+                setTypeCours(split[1], cours);
+                if (split.length > 2) {
+                    cours.setEnseignant(split[2]);
                 }
             }
-            if (split.length > 3) {
-                cours.setEnseignant(split[3]);
-            }
+        } else if (split.length > 0) {
+            // une seule chaine
+            cours = new Cours(split[0]);
+        } else {
+            return null;
+        }
 
-            // calcul horaire
-            int decalage = cell.getRowIndex() - rowNumber;
-            for(int debutC = 0; debutC<nbDebutCours; debutC++) {
-                if (debutC == nbDebutCours-1) {
-                    setHoraireEtGroupeCours(debutC, cours, decalage);
-                }
-                else if (debutCours[debutC]<=decalage && debutCours[debutC+1]>decalage) {
-                    setHoraireEtGroupeCours(debutC, cours, decalage);
-                    break;
-                }
+        // calcul horaire
+        int decalage = cell.getRowIndex() - rowNumber;
+        for(int debutC = 0; debutC<nbDebutCours; debutC++) {
+            if (debutC == nbDebutCours-1) {
+                setHoraireEtGroupeCours(debutC, cours, decalage);
+            }
+            else if (debutCours[debutC]<=decalage && debutCours[debutC+1]>decalage) {
+                setHoraireEtGroupeCours(debutC, cours, decalage);
+                break;
             }
         }
 
@@ -357,12 +394,40 @@ public class M1Reader {
         return cours;
     }
 
+    private static void setTypeCours(String split, Cours cours) {
+        split = split.trim();
+        switch (split) {
+            case "Cours":
+                cours.setType(TypeCours.TYPE_COURS);
+                break;
+            case "TD":
+            case "TD1":
+            case "TD2":
+            case "Groupe A":
+            case "Groupe B":
+            case "Groupe A :":
+            case "Groupe B :":
+                cours.setType(TypeCours.TYPE_TD);
+                break;
+            case "TP":
+                cours.setType(TypeCours.TYPE_TP);
+                break;
+            case "EXAMEN":
+            case "examen":
+                cours.setType(TypeCours.TYPE_CONTROLE);
+                break;
+            default:
+                cours.setType(TypeCours.TYPE_AUTRE);
+        }
+    }
+
     private void setHoraireEtGroupeCours(int debutC, Cours cours, int decalage) {
         System.out.println(debutCoursHoraires[debutC]);
         ZonedDateTime dateCours = date.plusHours(debutCoursHoraires[debutC]/100);
         dateCours = dateCours.plusMinutes(debutCoursHoraires[debutC]%100);
         cours.setDebut(dateCours);
-        cours.setDuree(2.0f); // A Modifier !!!!
+        if (cours.getDuree() == 0.0)
+            cours.setDuree(2.0f); // Durée par défaut
 
         if(cours.getType()==TypeCours.TYPE_TD) {
             for (int debTD = 0; debTD < nbGroupesTD; debTD++) {
@@ -377,60 +442,6 @@ public class M1Reader {
                     cours.setGroupe(groupeTPNames[debTP]);
                 }
             }
-        }
-    }
-
-
-    private void gestionTypeCours(String intitule, Cours cours, Cell ignoredCellIntitule) {
-        if (intitule.contains(" C"))
-            cours.setType(TypeCours.TYPE_COURS);
-        else if (intitule.contains(" TD"))
-            cours.setType(TypeCours.TYPE_TD);
-        else if (intitule.contains(" TP"))
-            cours.setType(TypeCours.TYPE_TP);
-        else if (intitule.contains(" BE"))
-            cours.setType(TypeCours.TYPE_BE);
-        else if (intitule.contains(" Projet"))
-            cours.setType(TypeCours.TYPE_PROJET);
-
-        // Contrôle
-        String couleur = colorReader.getColor(rowNumber, colNumber);
-        if (couleur != null && !couleur.equals("0")) {
-
-            switch (couleur) {
-                case "FFFFC000": // Ingé
-                case "9": // Ingé autre tinte de vert :(
-                case "7": // Ingé M1
-                    cours.setType(TypeCours.TYPE_INGE);
-                    break;
-                case "FF92D050": // L3 IRT
-                    cours.setType(TypeCours.TYPE_IRT);
-                    break;
-                case "FFFF85FE": // Alternance M1
-                    cours.setType(TypeCours.TYPE_ALTERNANCE);
-                    break;
-                case "8": // Non alts M1
-                    cours.setType(TypeCours.TYPE_NONALT);
-                    break;
-                case "FFFFFF00": // Contrôle
-                    cours.setType(TypeCours.TYPE_CONTROLE);
-                    break;
-                case "FFFF0000":
-                    cours.setType(TypeCours.TYPE_PROBLEME);
-                    break;
-                case "FF00FA00":
-                    cours.setType(TypeCours.TYPE_BE);
-                    break;
-                case "FF00FFFF": // dev durable M1/M2
-                case "FFFF40FF": // M1 RT
-                case "FFFFFFFF": // blanc
-                    cours.setType(TypeCours.TYPE_AUTRE);
-                    break;
-                default:
-                    cours.setType(TypeCours.TYPE_AUTRE);
-                    System.err.println("WARNING Nouvelle Couleur pour " + intitule + " : " + couleur);
-            }
-            System.out.println("Couleur de " + intitule + " : " + couleur);
         }
     }
 
@@ -455,11 +466,39 @@ public class M1Reader {
     public static void main(String[] args) throws Exception {
         M1Reader reader;
 
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(ZonedDateTime.class, new TypeAdapter<ZonedDateTime>() {
+                    @Override
+                    public void write(JsonWriter out, ZonedDateTime value) throws IOException {
+                        out.value(value.toString());
+                    }
+
+                    @Override
+                    public ZonedDateTime read(JsonReader in) throws IOException {
+                        return ZonedDateTime.parse(in.nextString());
+                    }
+                })
+                .enableComplexMapKeySerialization()
+                .create();
+
         reader = new M1Reader("24-25EDT S8 M1 MIAGE-propo- 1.xlsx", 0);
 
-        Calendrier calendrier = reader.traiterFichier();
+        Calendrier calendrier;
 
-        reader.close();
+        if (reader == null) {
+            // re-read JSON file
+            FileReader fileReader = new FileReader("calendrierM1Miage.json");
+            calendrier = gson.fromJson(fileReader, Calendrier.class);
+            fileReader.close();
+        } else {
+            calendrier = reader.traiterFichier();
+            reader.close();
+
+            String json = gson.toJson(calendrier);
+            FileWriter fileWriter = new FileWriter("calendrierM1Miage.json");
+            fileWriter.write(json);
+            fileWriter.close();
+        }
 
         Scanner scanner = new Scanner(System.in);
 
@@ -468,7 +507,7 @@ public class M1Reader {
         boolean numSemaines = true;
         boolean joursSemaine = true;
         int i = 0;
-
+        boolean all = false;
         while(true) {
             System.out.println("0 - fin");
             System.out.println("1 - tout");
@@ -482,27 +521,32 @@ public class M1Reader {
             System.out.println("Votre choix :");
             int choice = scanner.nextInt();
             scanner.nextLine();
+
             switch(choice) {
                 case 0 -> {
                     return;
                 }
                 case 1 -> {
                     matcher = new Cours(null);
+                    all = true;
                 }
                 case 2 -> {
                     System.out.println("Intitulé :");
                     String intitule = scanner.nextLine();
                     matcher.setIntitule(intitule);
+                    all = false;
                 }
                 case 3 -> {
                     System.out.println("Enseignant :");
                     String enseignant = scanner.nextLine();
                     matcher.setEnseignant(enseignant);
+                    all = false;
                 }
                 case 4 -> {
                     System.out.println("Groupe :");
                     String groupe = scanner.nextLine();
                     matcher.setGroupe(groupe);
+                    all = false;
                 }
                 case 5 -> {
                     System.out.println("Types :");
@@ -512,6 +556,7 @@ public class M1Reader {
                     System.out.println("Votre choix :");
                     String type = scanner.nextLine();
                     matcher.setType(TypeCours.valueOf(type));
+                    all = false;
                 }
                 case 6 -> {
                     matchAny = !matchAny;
@@ -525,9 +570,7 @@ public class M1Reader {
                     joursSemaine = !joursSemaine;
                     System.out.println("Switch joursSemaine maintenant : "+joursSemaine);
                 }
-                default -> {
-                    System.out.println("Choix non valide");
-                }
+                default -> System.out.println("Choix non valide");
             }
             System.out.println("Matcher " + matcher);
 
@@ -537,7 +580,7 @@ public class M1Reader {
 
             ListWriter listWriter = new ListWriter(calendrier);
 
-            listWriter.generate(new File("m1miage"+i+".xlsx"), matcher, matchAny, numSemaines, joursSemaine);
+            listWriter.generate(new File("m1miage"+i+".xlsx"), all?null:matcher, matchAny, numSemaines, joursSemaine);
 
             StatisticsGenerator statisticsGenerator = new StatisticsGenerator(calendrier);
 
